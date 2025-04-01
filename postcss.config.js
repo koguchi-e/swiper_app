6a36fdc0a74a3dd7faaaf0a1592c1d341db8b8c6
Erika.K <koguchi.e915@gmail.com>
2025-04-01 17:54:21 +0900

    [Update]スライドショー表示されない

--- postcss.config.js
+++ postcss.config.js
@@ -3,10 +3,11 @@ module.exports = {
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
+      stage: 3,
       autoprefixer: {
         flexbox: 'no-2009'
-      },
-      stage: 3
+      }
     })
   ]
-}
+};
+
